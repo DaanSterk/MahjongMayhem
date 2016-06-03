e8162d243c968e1d849dfcfb7f443e05369c92e3
DaanSterk <daan-sterk@hotmail.com>
2016-06-03 13:55:54 +0200

    trying to get ui-router working...

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,19 @@
 require('angular/angular');
+require('angular-ui-router');
 
 // Create your app
-var app = angular.module('MahjongMayhem', []);
+var app = angular.module('MahjongMayhem', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('games', {
+            url: "/games",
+            templateUrl: "templates/games/index.html",
+            controller: "GamesCtrl"
+        });
+
+    // For any unmatched url, send to /index
+    $urlRouterProvider.otherwise("/");
+
+});
--- app/js/controllers/AppCtrl.js
+++ app/js/controllers/AppCtrl.js
@@ -1,3 +1,6 @@
-angular.module('MahjongMayhem').controller('AppCtrl', ['$scope', function($scope) {
+angular.module('MahjongMayhem').controller('AppCtrl', ['$scope', '$state', function($scope, $state) {
     $scope.welcome_msg = 'Welcome to Mahjong Mayhem!';
+    $scope.showGames = function() {
+        $state.go('games');
+    }
 }]);
