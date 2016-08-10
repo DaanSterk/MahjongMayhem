78e9f25d67c210695fdae4838bb72ade9368e76b
Jeffrey Vervoort <jeffreyvervoort@gmail.com>
2016-08-10 15:56:55 +0200

    testen per controller opgesplitst

--- test/game.spec.js
+++ test/game.spec.js
@@ -51,163 +51,52 @@ describe("GameCtrl", function() {
                 expect(gameController.isTileFromTheSameType(firstTile, secondTile), false);
             });
         });
-    });
-});
 
-describe("GamesCtrl", function() {
-    var gamesController;
-    var gamesService;
-    var createNewController;
-    var httpBackend;
-    var scope;
-    var game;
+        describe("Can the tile be selected", function () {
+            it("Tile can be selected", function () {
+                var tileArray = [
+                    {tile: {tile: {id: 1, suit: "bamboo", name: "1", matchesWholeSuit: false}, xPos: 0, yPos: 0, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "dragon", name: "red", matchesWholeSuit: false}, xPos: 1, yPos: 1, zPos:0}}
+                ];
 
-    // initialize the app
-    beforeEach(module('MahjongMayhem'));
+                gameController.tiles = tileArray;
 
-    // Inject the modules and get them in global variables
-    beforeEach(inject(function($rootScope, $controller, $httpBackend, $injector){
-        // The scope for the controller
-        scope = $rootScope.$new();
-        // Get the service which will be injected
-        gameService = $injector.get('gamesService');
-        // For mocking the backend
-        httpBackend = $httpBackend;
-
-        localStorage.setItem("user.username", "BugsBunny");
-
-        // This is the controller we're going to test
-        gamesController = $controller('GamesCtrl', { $scope: scope });
-    }));
-
-    describe("Testing the GamesController", function () {
-        describe("Is the game joinable", function () {
-            it("Should be able to join", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"a.ketchum@student.avans.nl",name:"Ash Ketchum",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"a.ketchum@student.avans.nl",name:"Ash Ketchum",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isJoinable(game), true);
+                expect(gameController.canTheTileBeSelected(tileArray[0]), true);
             });
 
-            it("Should not be able to join", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
+            it("There is a tile laying on top of this one", function () {
+                var tileArray = [
+                    {tile: {tile: {id: 1, suit: "bamboo", name: "1", matchesWholeSuit: false}, xPos: 0, yPos: 0, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "dragon", name: "red", matchesWholeSuit: false}, xPos: 0, yPos: 0, zPos:1}}
+                ];
 
-                expect(gamesController.isJoinable(game), false);
-            });
-        });
+                gameController.tiles = tileArray;
 
-        describe("Is it possible to start the game", function () {
-            it("Should be able start the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isStartable(game), true);
+                expect(gameController.canTheTileBeSelected(tileArray[0]), false);
             });
 
-            it("Should not be able to start the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:13,minPlayers:4,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
+            it("Tile is horizontally surrounded", function () {
+                var tileArray = [
+                    {tile: {tile: {id: 1, suit: "bamboo", name: "1", matchesWholeSuit: false}, xPos: 0, yPos: 0, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "dragon", name: "red", matchesWholeSuit: false}, xPos: 0, yPos: 1, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "circle", name: "2", matchesWholeSuit: false}, xPos: 0, yPos: 2, zPos:0}}
+                ];
 
-                expect(gamesController.isStartable(game), false);
-            });
-        });
+                gameController.tiles = tileArray;
 
-        describe("Is it possible to delete the game", function () {
-            it("Should be able to play", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isDeletable(game), true);
+                expect(gameController.canTheTileBeSelected(tileArray[1]), false);
             });
 
-            it("Should not be possible to delete the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:13,minPlayers:4,state:"playing",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
+            it("Tile is vertically surrounded", function () {
+                var tileArray = [
+                    {tile: {tile: {id: 1, suit: "bamboo", name: "1", matchesWholeSuit: false}, xPos: 0, yPos: 0, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "dragon", name: "red", matchesWholeSuit: false}, xPos: 1, yPos: 0, zPos:0}},
+                    {tile: {tile: {id: 2, suit: "circle", name: "2", matchesWholeSuit: false}, xPos: 2, yPos: 0, zPos:0}}
+                ];
 
-                expect(gamesController.isDeletable(game), false);
-            });
-        });
+                gameController.tiles = tileArray;
 
-        describe("Is it possible to play", function () {
-            it("Should be able start the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isPlayable(game), true);
-            });
-
-            it("Should not be possible to play the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:13,minPlayers:4,state:"playing",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isPlayable(game), false);
-            });
-        });
-
-        describe("Is it possible to spectate", function () {
-            it("Should be able start the game", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:7,minPlayers:1,state:"playing",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isSpectatable(game), true);
-            });
-
-            it("Should not be possible to spectate", function () {
-                var game = {_id:"57a9f3ee1d610d11007b5a0c",
-                    createdBy:{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0},
-                    createdOn:"2016-08-09T15:17:02.045Z",
-                    gameTemplate:{_id:"Shanghai",__v:0,id:"Shanghai"},
-                    __v:0,
-                    players:[{_id:"b.bugs@student.avans.nl",name:"Bugs Bunny",__v:0,$$hashKey:"object:3777"}],
-                    maxPlayers:13,minPlayers:4,state:"open",id:"57a9f3ee1d610d11007b5a0c",$$hashKey:"object:3671"};
-
-                expect(gamesController.isSpectatable(game), false);
+                expect(gameController.canTheTileBeSelected(tileArray[1]), false);
             });
         });
     });
